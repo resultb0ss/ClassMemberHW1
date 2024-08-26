286902aaab87788a8f3ba14f2d1cb805d45a2ea6
Admin <reteamboss@gmail.com>
2024-08-26 18:04:30 +0300

    Задание 1

--- src/Helicopter.kt
+++ src/Helicopter.kt
@@ -1,2 +1,14 @@
 class Helicopter {
+
+    var name: String = ""
+    var weight: Int = 0
+    var height: Int = 0
+    var speed: Int = 0
+
+    constructor(name: String, weight: Int, height: Int, speed: Int) {
+        this.name = name
+        this.weight = weight
+        this.height = height
+        this.speed = speed
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    println("Hello World!")
+    var helicopter0ne = Helicopter("К-52",100,230,400)
+
+    println("Спроектировали вертолт с названием ${helicopter0ne.name} скоростью полета ${helicopter0ne.speed} весом" +
+            "${helicopter0ne.weight} и высотой полета ${helicopter0ne.height}")
+
 }
